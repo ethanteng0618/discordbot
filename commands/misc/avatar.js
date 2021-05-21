763e109e8000c59c61d5c3e13bcf79e63166e5ea
TheExoticDude <60494490+TheExoticDude@users.noreply.github.com>
2021-05-21 09:57:13 -0400

    test done

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -5,7 +5,6 @@ module.exports = {
 		const user = message.mentions.users.first()
 		if (!message.mentions.users.size) {
 			message.channel.send('<@' + message.author.id + '>\'s avatar: ' + message.author.displayAvatarURL());
-			message.channel.send('lol noob')
 		} else {
 			message.channel.send(`<@${user.id}>'s avatar: ${user.displayAvatarURL()}`)
 		}
