288cd123bf4070d2bc2afd692fd461410a6c35cd
KingBP25 <84361129+KingBP25@users.noreply.github.com>
2021-11-11 07:16:53 -0500

    Create freshcut.js

---
+++ commands/misc/freshcut.js
@@ -0,0 +1,7 @@
+module.exports = {
+   name: 'freshcut'
+   description: 'makes freshcut'
+   excecute(message,args) {
+      message.channel.send('https://discord.com/channels/845438790020890635/867587128216911922/904176087674781696')
+   }
+}
