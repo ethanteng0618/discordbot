17431ff1cf90c65c35d7b21a08d7592325545e9d
KingBP25 <84361129+KingBP25@users.noreply.github.com>
2021-11-11 07:26:26 -0500

    Update freshcut.js

--- commands/misc/freshcut.js
+++ commands/misc/freshcut.js
@@ -1,7 +1,7 @@
 module.exports = {
-   name: 'freshcut'
-   description: 'makes freshcut'
-   excecute(message,args) {
-      message.channel.send('https://cdn.discordapp.com/attachments/867587128216911922/904176082343829575/B9212C5E-B9CF-40BD-B44A-2BE356D162CB.png')
-   }
+    name: 'freshcut',
+    description: 'makes freshcut',
+    execute(message, args) {
+        message.channel.send('https://cdn.discordapp.com/attachments/867587128216911922/904176082343829575/B9212C5E-B9CF-40BD-B44A-2BE356D162CB.jpg')
+    }
 }
