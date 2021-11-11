4bbd0c5ae4c5246593c9eeecf3959cbfe1d0c00e
Daniel <ultraexotic@icloud.com>
2021-11-10 22:32:11 -0500

    rewrote guess.js, created aiden.js
    rewrote guess.js and made it work. spaghetti code.

--- commands/fun/guess.js
+++ commands/fun/guess.js
@@ -1,33 +1,42 @@
+const discordjs = require('discord.js')
+
 module.exports = {
     name: 'guess',
     description: "guess a number 1-100",
     execute(message, args) {
-        var mes = message.content.split(" ");
-        message.reply('Picking a random number between 1 and 100');
-        num = Math.floor((Math.random() * 100) + 1);
-        guesses = 0;
-            if (num == 0) 
-            {
-                message.reply('Picking a random number between 1 and 100');
-                num = Math.floor((Math.random() * 100) + 1);
-                guesses = 0;
-            }
-            else if (mes[1] == num) 
-            {
-                guesses++;
-                message.reply('You got it! Only took ' + guesses + ' tries.');
-                message.reply('Picking a random number between 1 and 100');
-                num = Math.floor((Math.random() * 100) + 1);
-                guesses = 0;
-            }
-            else if (mes[1] < num) {
-                message.reply(mes[1] + ' is too low shitter');
-                guesses++;
+            filter = m => m.author.id === message.author.id
 
-            }
-            else if (mes[1] > num) {
-                message.reply(mes[1] + ' is too high shitter');
-                guesses++;
-            }
+            var guesses = 5
+            const num = Math.floor(Math.random() * 100) + 1
+
+            console.log(num)
+
+            const collector = new discordjs.MessageCollector(message.channel, filter, {
+                time: 120000
+            })
+
+            message.reply('guess a number between 1-100')
+            collector.on('collect', m => {
+                const guess = parseInt(m.content)
+                if (guesses > 0) {
+                    if (guess === num) {
+                        m.reply('damn bro you got it')
+                        collector.stop()
+                    } 
+                    else if (guess > num) m.reply('too high 😡😡😡😡😡(' + guesses-- + ' guesses left)')
+                    else if (guess < num) m.reply('too low 😴😫😫😫😫(' + guesses-- + ' guesses left)')
+                    else {
+                        m.reply('a number between 1-100🙄')
+                        collector.stop()
+                    }
+                } else {
+                    m.reply('number was ' + num + ' and you guess like shit')
+                    collector.stop()
+                }
+            })
+
+            collector.on('end', collected =>{
+                console.log(`${collected.size}`)
+            })
         }
     }
---
+++ commands/misc/aiden.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'aiden',
+    description: 'makes aiden',
+    execute(message, args) {
+        message.channel.send('https://cdn.discordapp.com/attachments/870777808656408718/871203906951196692/deepfried.png')
+    }
+}
