f306c3816b0406268423a20ee08a4d791f9e84c6
KingBP25 <84361129+KingBP25@users.noreply.github.com>
2021-11-11 07:20:57 -0500

    Update freshcut.js

--- commands/misc/freshcut.js
+++ commands/misc/freshcut.js
@@ -2,6 +2,6 @@ module.exports = {
    name: 'freshcut'
    description: 'makes freshcut'
    excecute(message,args) {
-      message.channel.send('https://cdn.discordapp.com/attachments/867587128216911922/904176082343829575/B9212C5E-B9CF-40BD-B44A-2BE356D162CB.jpg')
+      message.channel.send('https://cdn.discordapp.com/attachments/867587128216911922/904176082343829575/B9212C5E-B9CF-40BD-B44A-2BE356D162CB.png')
    }
 }
